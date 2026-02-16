3c30d11eff7cf4a233fea176475b50c13a546246
Brenden Chu <brendenchu@gmail.com>
2026-02-16 12:36:05 -0800

    Enhance demo reset command to clean visitor data and reset demo user credentials

--- routes/console.php
+++ routes/console.php
@@ -1,1 +1,8 @@
 <?php
+
+use Illuminate\Support\Facades\Schedule;
+
+Schedule::command('demo:reset')
+    ->daily()
+    ->timezone('America/Vancouver')
+    ->when(fn () => config('demo.enabled'));
